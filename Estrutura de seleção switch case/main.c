ae23f8231ed2ac1b32031450685b325ee6147c43
g8u16 <121985181+g8u16@users.noreply.github.com>
2023-01-06 14:23:59 -0300

    switch case

---
+++ Estrutura de seleção switch case/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int opcao;
+
+    printf("1 - Cadastrar produto\n2 - Vender produto\n3 - Buscar produto\n4 - Imprimir\n5 - Sair\n");
+    scanf("%d", &opcao);
+
+    switch(opcao) {
+    case 1:
+        printf("Cadastrando produto. \n");
+        break;
+    case 2:
+        printf("Vendendo produto. \n");
+        break;
+    case 3:
+        printf("Buscando produto. \n");
+        break;
+    case 4:
+        printf("Imprimindo. \n");
+        break;
+    case 5:
+        printf("Saindo..\n");
+        break;
+    default:
+        printf("Opcao invalida!\n");
+
+
+
+    }
+    return 0;
+}
