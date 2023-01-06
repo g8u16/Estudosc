4a92b2220bf5831240cf61936e638808ec81633a
g8u16 <121985181+g8u16@users.noreply.github.com>
2023-01-06 13:25:17 -0300

    okay

--- ternario/main.c
+++ ternario/main.c
@@ -4,17 +4,26 @@
 int main()
 {
 
-int x;
-printf("Digite um valor: ");
-scanf("%d", &x);
+//int x;
+//printf("Digite um valor: ");
+//scanf("%d", &x);
 
 
-!(x < 0) ? printf("O valor e negativo") : printf("O valor e positivo");
+//!(x < 0) ? printf("O valor e negativo") : printf("O valor e positivo");
 
    //x < 0 ? printf("O valor e negativo") :
        //x > 0 ? printf("O valor e positivo") : printf("O valor e zero");
 
     //x <= 0 ? printf("Value okay") : printf("Value very nice");
+
+
+    int y;
+
+    printf("\nDigite um valor: ");
+    scanf("%d", &y);
+
+    y == 0 ? printf("\nValor nulo\n"):
+        y < 0 ? printf("\nO valor e negativo\n") : printf("\nO valor e positivo\n");
 
 
 
